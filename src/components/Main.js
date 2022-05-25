4bffde53023056cf289fb015432b97e28bf1a613
André Torres <andretorresjava@gmail.com>
2022-05-24 19:27:05 -0500

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import { ReactComponent as Adventure } from '../assets/icons/undraw_adventure_4hum 1.svg';
+
+const Main = () => {
+  return (
+    <div className="container">
+        <h1 className="title">country quiz</h1>
+        <div className="content">
+            <Adventure />
+            <p>Texto de prueba</p>
+        </div>
+    </div>
+  )
+}
+
+export default Main;
